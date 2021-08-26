353575b61869d660e288ab45be83839e425e6796
kjh2623jh <kjh2623kjh@gmail.com>
2021-08-27 04:18:03 +0900

    Add files via upload

---
+++ 근무표 짜기.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+day = list(map(int, input().split()))
+maxPeople = list(map(int, input().split()))
+dayDic = {}
+for i, j in enumerate(day):
+    dayDic.update({i+1:j})
+a = []
+
+for i in maxPeople:
+    print(i, end=' ')
+    for j in sorted(dayDic.values())[-i:]: print(j, end=' ')
+    print()
