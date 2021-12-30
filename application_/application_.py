f0debc2ac153d3da735d53a8d40d14a67d0f206e
kjh2623jh <kjh2623kjh@gmail.com>
2021-12-31 08:03:57 +0900

    application_

--- application_/application_.py
+++ application_/application_.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QIcon
 
 class MyApp(QWidget):
     def __init__(self):
@@ -8,8 +9,8 @@ class MyApp(QWidget):
 
     def initUI(self):
         self.setWindowTitle('my first application')
-        self.move(300, 300)
-        self.resize(400, 200)
+        self.setWindowIcon(QIcon('app_icon.png'))
+        self.setGeometry(300, 300, 300, 200)
         self.show()
     
 
@@ -17,4 +18,4 @@ class MyApp(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
