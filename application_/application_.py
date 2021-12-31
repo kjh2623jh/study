8d53fb1faa04139dd778811bdc49030c2c1d69b0
kjh2623jh <kjh2623kjh@gmail.com>
2022-01-01 04:05:56 +0900

    aplication_

--- application_/application_.py
+++ application_/application_.py
@@ -1,21 +1,32 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
-from PyQt6.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget, QToolTip
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import QCoreApplication
+
 
 class MyApp(QWidget):
+
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('my first application')
-        self.setWindowIcon(QIcon('app_icon.png'))
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Quit', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.move(50, 50)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
+
+        self.setWindowTitle('My First Application')
+        self.setWindowIcon(QIcon('app_ico.ico'))
         self.setGeometry(300, 300, 300, 200)
         self.show()
-    
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyApp()
-    sys.exit(app.exec())
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
