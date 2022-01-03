20f2db9d7b7b58e82313a7db781c6ad7fcf2a079
kjh2623jh <kjh2623kjh@gmail.com>
2022-01-04 05:40:04 +0900

    first

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GHelllo World");
+            string userName;
+            Console.WriteLine("What is your name?");
+            userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}.");
+        }
+    }
+}
