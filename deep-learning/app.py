687485ad4329c2bc0205bf62d202b322732b444b
nemi <kjh2623kjh@gmail.com>
2022-04-12 01:35:03 +0900

    외 않 됌?

--- deep-learning/app.py
+++ deep-learning/app.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-a = Image.open('image.jpg')
+a = Image.open('테스트.jpg')
 txt = pytesseract.image_to_string(a, lang='kor')
 
 print(txt)
