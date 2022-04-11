327d78e6541f83181d45529b5a36f41f68bedbc1
nemi <kjh2623kjh@gmail.com>
2022-04-12 01:26:49 +0900

    a

--- deep-learning/app.py
+++ deep-learning/app.py
@@ -1,9 +1,9 @@
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-a = Image.open('이미지.jpg')
+a = Image.open('image.jpg')
 txt = pytesseract.image_to_string(a, lang='kor')
 
 print(txt)
