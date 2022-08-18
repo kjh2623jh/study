535d9e987dcb40daa3da661f15124395694eda72
nemi <kjh2623kjh@gmail.com>
2022-08-19 04:10:57 +0900

    23/200

--- NYPC/2022-인류의 적 모기 퇴치.py
+++ NYPC/2022-인류의 적 모기 퇴치.py
@@ -1,14 +1,29 @@
 m, n = map(int, input().split())
 Map = []
-cnt=-1
+cnt=-2
+cnt_=-1
 Cnt = []
 
 for i in range(m):
     Map.append([*map(int, input().split())])
 
-for i in range(-n, m-n+1):
-    for j in range(n, m+n+1):
-        [y for x in Map[i:j+1] for y in x[i:j+1]]
+for i in range(m):
+    for j in range(m):
+        cnt+=2
+        cnt_+=2
+        Cnt.append(Map[i][j])
+        Cnt.append(Map[i][j])
+        for x in range(-n, n+1):
+            if not x:
+                continue
+            if i+x>=0 and i+x<m:
+                Cnt[cnt]+=Map[i+x][j]
+                if j+x>=0 and j+x<m:
+                    Cnt[cnt_]+=Map[i+x][j+x]
+            if j+x>=0 and j+x<m:
+                Cnt[cnt]+=Map[i][j+x]
+                if i+x>=0 and i+x<m:
+                    Cnt[cnt_]+=Map[i+x][j+x]
 
 print(max(Cnt))
 # for i in range(0, m*m, m):
