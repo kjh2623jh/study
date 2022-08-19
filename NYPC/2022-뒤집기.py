acdbecae5604d997ff4dc590b01a3013470ef5ed
훈제김치 <kjh2623kjh@gmail.com>
2022-08-19 22:25:41 +0900

    0

--- NYPC/2022-뒤집기.py
+++ NYPC/2022-뒤집기.py
@@ -1,19 +1,27 @@
 n = int(input())
 players = [int(x) for x in input().split()]
-score = []
+score = [0]
 
-for lo in range(n-1):
-    for hi in range(lo+1, n):
-        score.append(sum(players[lo:hi]))
+num=0
+for i in players:
+    num+=i
+    if num<0:
+        num=0
+    elif num>score[-1]:
+        score[-1]=num
 
-for i in range(n-2):
-    for j in range(i+2, n):
-        players_ = players[0:i]+players[i:j][::-1]+players[j:n-1]
-        for lo in range(n-1):
-            for hi in range(lo+1, n):
-                score.append(sum(players_[lo:hi]))
+for i in range(n-1):
+    for j in range(i+2, n+1):
+        players_ = players[0:i]+players[i:j][::-1]+players[j:n]
+        score.append(0)
+        num=0
+        for p in players_:
+            num+=p
+            if num<0:
+                num=0
+            elif num>score[-1]:
+                score[-1]=num
 
-print(max(score))
-
-#이거 부르트 포스 아님 뭐로 풀어야하지?
-# 그리디?..
+r = max(score)
+print(r if r>0 else max(players))
+# 시간
