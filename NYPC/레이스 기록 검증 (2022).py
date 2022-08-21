9368f84be6172c38d6a43c80741b5df3c214aa0d
nemi <kjh2623kjh@gmail.com>
2022-08-22 04:49:58 +0900

    ./

--- NYPC/레이스 기록 검증 (2022).py
+++ NYPC/레이스 기록 검증 (2022).py
@@ -3,13 +3,13 @@ c = 0
 
 p = [0]*p
 for i in range(n):
-    a, b, c = map(int, input().split())
-    if c == 0 and p[b-1] == 0:
-        p[b-1]=a
-    elif a-p[b-1] < 60:
+    a, s, d = map(int, input().split())
+    if d == 0 and p[s-1] == 0:
+        p[s-1]=a
+    elif a-p[s-1] < 60:
         c = 1
-    elif p[b-1] != 0:
-        p[b-1] = 0
+    elif p[s-1] != 0:
+        p[s-1] = 0
     else:
         c = 1
 if sum(p) > 0:
