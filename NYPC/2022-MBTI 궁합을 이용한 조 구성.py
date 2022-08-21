9cacb1486eb5aca9886bcacae8e0fb9e06559424
nemi <kjh2623kjh@gmail.com>
2022-08-22 04:49:48 +0900

    ./

--- NYPC/2022-MBTI 궁합을 이용한 조 구성.py
+++ NYPC/2022-MBTI 궁합을 이용한 조 구성.py
@@ -1,23 +1,12 @@
 from collections import Counter
 
+['enfp', 'enfj', 'entp', 'entj', 'esfp', 'esfj', 'estp', 'estj', 'infp', 'infj', 'intp', 'intj', 'isfp', 'isfj', 'istp', 'istj']
+
 for _ in range(int(input())):
-    number, i, n, f, p = map(int, input().split())
-    pp = []
+    number, m, b, t, i = map(int, input().split())
     cnt = []
-    mbti = Counter()
-    for aa in input().split():
-        mbti[aa] +=1
-    for aa in mbti:
-        if mbti[aa]%2:
-            pp.append(mbti[aa])
-    
-    '''
-    mbti = [i,n,f,p]
-    for aa in range(0, len(pp)-1):
-        for bb in range(aa+1, len(pp)):
-            c=0
-            for x in range(4):
-                if pp[aa][x] != pp[bb][x]:
-                    c+=mbti[x]
-            cnt.append(c)
-    '''
+    mbti = Counter(input().split())
+    pp = [x for x in mbti if mbti[x]%2]
+    mbti = [m,b,t,i]
+    for n, i in sorted(zip(mbti, range(4)), reverse=True):
+        ...
